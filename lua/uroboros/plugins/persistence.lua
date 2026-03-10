4be485ac4cd62e4868e248a39965193aa3a9bcde
Prasurjya Pratim Talukdar <128120000+omnimistic@users.noreply.github.com>
2026-03-11 02:20:52 +0530

    Delete lua/uroboros directory

--- lua/uroboros/plugins/persistence.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "folke/persistence.nvim",
-  event = "BufReadPre", -- only start session tracking when you actually open a file
-  opts = {
-    -- Automatically saves the session every time you change a buffer
-  }
-}
